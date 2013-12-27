93bbe9120b9a827014c0c7dd152904881487ea78
Rimi07 <rimi1@rimi-07-xiaochuan.local>
2013-12-27 08:44:40 +0800

    feature branch

---
+++ 1227/1227/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  1227
+//
+//  Created by Rimi07 on 13-12-27.
+//  Copyright (c) 2013年 cuan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
