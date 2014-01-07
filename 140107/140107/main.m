07a672c7bc0a1887bd931ab6a057b06572fb5f13
atcuan <atcuan@gmail.com>
2014-01-07 08:54:01 +0800

    0106 t3.c

---
+++ 140107/140107/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  140107
+//
+//  Created by Rimi07 on 14-1-7.
+//  Copyright (c) 2014年 cuan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    
+    return 0;
+}
+
