f2d6dc2f98bad462160ef1b41188da4228328867
atcuan <atcuan@gmail.com>
2014-01-14 11:54:50 +0800

    markdown syntax

---
+++ OC140114/OC140114/Student.h
@@ -0,0 +1,13 @@
+//
+//  Student.h
+//  OC140114
+//
+//  Created by cuan on 14-1-14.
+//  Copyright (c) 2014年 cuan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Student : NSObject
+- (void)dealloc;
+@end
