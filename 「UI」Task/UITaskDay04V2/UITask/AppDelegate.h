ac800ba8030e813477432d1aea2045cb9d96c48d
atcuan <atcuan@gmail.com>
2014-02-11 08:30:37 +0800

    daily push

--- 「UI」Task/UITaskDay04V2/UITask/AppDelegate.h
+++ 「UI」Task/UITaskDay04V2/UITask/AppDelegate.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  AppDelegate.h
 //  UITask
 //
 //  Created by cuan on 14-1-22.
@@ -8,8 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface RootViewController : UIViewController
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
 
-@property (assign, nonatomic, getter = isLogined) BOOL logined;
+@property (strong, nonatomic) UIWindow *window;
 
 @end
---
+++ 「UI」Task/UITaskDay04V2/UITask/RandomViewController.h
@@ -0,0 +1,13 @@
+//
+//  RandomViewController.h
+//  UITask
+//
+//  Created by cuan on 14-2-10.
+//  Copyright (c) 2014年 cuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RandomViewController : UIViewController
+
+@end
