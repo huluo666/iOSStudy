8e423d8a22f6f89ed3e4b1ef4e70cdfc49f54eb2
atcuan <atcuan@gmail.com>
2014-03-05 21:10:28 +0800

    UIPickerView

---
+++ 「UI」UIPickerView/「UI」UIPickerView/DDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DDAppDelegate.h
+//  「UI」UIPickerView
+//
+//  Created by 萧川 on 3/5/14.
+//  Copyright (c) 2014 CUAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 「UI」UIPickerView/「UI」UIPickerView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  「UI」UIPickerView
+//
+//  Created by 萧川 on 3/5/14.
+//  Copyright (c) 2014 CUAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DDAppDelegate class]));
+    }
+}
